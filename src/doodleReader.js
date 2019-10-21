5ad502b410a6dd0a6bf4291750bfd0b923525a6e
Kevin Atkins <atkinkev@oregonstate.edu>
2019-10-21 18:12:02 -0500

    Separating image processing out of index.js. Beginning image data processing

--- src/doodleReader.js
+++ src/doodleReader.js
@@ -1,12 +1,30 @@
-var jsfeat = require('jsfeat');
-var cannyEdgeDetector = require('canny-edge-detector');
-var Image = require('image-js');
-var testImage = require('./assets/doodles/test_doodle.jpg');
 
-export async function toGreyScale(){
+function readImage(){
+	var image = new Image();
+	image.crossOrigin = "anonymous";
+	image.onload = function () {
 
-	let image = await Image.load('./assets/doodles/test_doodle.jpg');
-	let grey = image.grey().resize({width: 200}).rotate(30);
-	return grey.save('newimage.png');
+	var canvas=document.getElementById("canvas");
+	canvas.style.display = "none";
+	var ctx=canvas.getContext("2d");
+	var cw=canvas.width;
+	var ch=canvas.height;
+
+	ctx.drawImage(image, canvas.width / 2 - image.width / 2, canvas.height / 2 - image.height / 2);
+
+	var imageData = ctx.getImageData(0, 0, cw, ch);
+	var data = imageData.data;
+
+	for (var x = 0; x < cw; x++){
+	  for(var y = 0; y < ch; y++){
+
+	  }
+	}
+	console.log('All done');
+	// canvas related variables
+	}
+	image.src = "edgeImage.png"
 }
 
+
+module.exports.readImage = readImage;
