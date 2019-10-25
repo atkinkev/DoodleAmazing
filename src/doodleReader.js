a058ff8817d52224103200a6456734a769e68887
Kevin Atkins <atkinkev@oregonstate.edu>
2019-10-25 09:33:28 -0500

    Integrating game start flow with image capture and processing

--- src/doodleReader.js
+++ src/doodleReader.js
@@ -1,9 +1,12 @@
-function getEdgeCoordinates(){
+function getEdgeCoordinates(doodleImage){
 	var image = new Image();
 	image.crossOrigin = "anonymous";
 	image.onload = function () {
 		var canvas=document.getElementById("canvas");
 		canvas.style.display = "none";
+		canvas.width = image.width;
+		canvas.height = image.height;
+
 		var ctx=canvas.getContext("2d");
 		var cw=canvas.width;
 		var ch=canvas.height;
@@ -12,34 +15,47 @@ function getEdgeCoordinates(){
 
 		var imageData = ctx.getImageData(0, 0, cw, ch);
 		var data = imageData.data;
-
 		for (var x = 0; x < cw; x++){
 		  for(var y = 0; y < ch; y++){
 
 		  	var pIndex = (x + y * cw) * 4;	// 4 indexes for each pixel RGBA
-		  	if (x == 300){
-		  		console.log('R: ' + data[pIndex] + ' G: ' + data[pIndex + 1] + ' B: ' + data[pIndex + 2] + " A:" + data[pIndex + 3]);
-			}		  
+		  	console.log('R: ' + data[pIndex] + ' G: ' + data[pIndex + 1] + ' B: ' + data[pIndex + 2] + " A:" + data[pIndex + 3]);	
 		  }
 		}
+		console.log('all done');
+		//canvas.remove();
 	}
-	image.src = "edgeImage.png"
+	image.src = doodleImage.src;
 }
 
-async function findEdges(){
+async function prepImage(){
 	const { Image } = require('image-js');
-	try{
-		let image = await Image.load('test_doodle.jpg');
-		let grey = image.grey();
-		//let edge = canny(grey);
-		//grey.save('superhappyfun.png');
-	}
-	catch(error){
-		console.error(error);
+	let image = await Image.load('test_doodle.jpg');
+	let grey = image.grey();
+	return grey;
+}
+
+function uploadImage(){
+	// Beginning of image upload
+	var inputElement = document.getElementById("pic");
+
+	inputElement.onchange = function(event) {
+
+	   var image = inputElement.files[0];
+	   var img = new Image();
+	   var reader = new FileReader();
+	   reader.onload = function(e){
+	    img.src = e.target.result;
+	    uploadedImage = e.target.result;
+	    inputElement.style.display = "none";
+	    getEdgeCoordinates(img);
+	   }
+	   reader.readAsDataURL(image);
 	}
 }
 
 module.exports.getEdgeCoordinates = getEdgeCoordinates;
-module.exports.findEdges = findEdges;
+module.exports.prepImage = prepImage;
+module.exports.uploadImage = uploadImage;
 
 // True black 255, 255, 255 ,255
--- src/index.js
+++ src/index.js
@@ -1,55 +1,41 @@
 import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
-import {getEdgeCoordinates} from './doodleReader';
-import {findEdges} from './doodleReader';
+// import logoImg from "./assets/logo.png";
+import {uploadImage} from './doodleReader';
+import {prepImage} from './doodleReader';
 const canny = require('canny-edge-detector');
 require("babel-polyfill");
 
-getEdgeCoordinates();
-findEdges();
+uploadImage();
 
-const config = {
-  type: Phaser.AUTO,
-  parent: "doodle-amazing",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
+function setup(){
+  const config = {
+    type: Phaser.AUTO,
+    parent: "doodle-amazing",
+    width: 800,
+    height: 600,
+    scene: {
+      preload: preload,
+      create: create
+    }
+  };
+
+  const game = new Phaser.Game(config);
+
+  function preload() {
+    this.load.image("logo", uploadImage);
   }
-};
 
-const game = new Phaser.Game(config);
+  function create() {
+    const logo = this.add.image(400, 150, "logo");
 
-function preload() {
-  this.load.image("logo", logoImg);
-}
-
-function create() {
-  const logo = this.add.image(400, 150, "logo");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
-}
-
-
-
-
-// Beginning of image upload
-var inputElement = document.getElementById("pic");
-
-inputElement.onchange = function(event) {
-   var image = inputElement.files[0];
-
-   var reader = new FileReader();
-   reader.onload = function(e){
-    var img = new Image();
-   }
+    this.tweens.add({
+      targets: logo,
+      y: 450,
+      duration: 2000,
+      ease: "Power2",
+      yoyo: true,
+      loop: -1
+    });
+  }
 }
 
