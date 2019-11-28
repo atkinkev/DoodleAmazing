e4caec22355629117e833cbc41fead55f050d973
Kevin Atkins <atkinkev@oregonstate.edu>
2019-11-28 11:46:18 -0600

    Enhanced collision functionality

--- src/GameScene.js
+++ src/GameScene.js
@@ -3,6 +3,7 @@ import ball from "./assets/imgs/ball.png";
 import wall from "./assets/imgs/black_pixel.png"
 import dpad from "./assets/imgs/pad.png";
 var _coordinates;
+var group;
 
 export default class GameScene extends Phaser.Scene {
 
@@ -25,24 +26,44 @@ export default class GameScene extends Phaser.Scene {
     _coordinates = coordinates["walls"];
     const sizingRatio = canvasHeight / coordinates["max_height"];
     this.cursors = this.input.keyboard.createCursorKeys();
-
+    var even =true;
+    var group = this.physics.add.staticGroup();
+    this.physics.world.setFPS(120);
+    console.log(this.physics.world.TILE_BIAS = 64);
     for(var coordinate of _coordinates){
+      even = !even;
       if(Math.abs(coordinate['X'] - coordinates["ball"][0]) < 20 && Math.abs(coordinate['Y'] - coordinates["ball"][1]) < 20){
         continue;
       }
       if(Math.abs(coordinate['X'] - coordinates["hole"][0]) < 20 && Math.abs(coordinate['Y'] - coordinates["hole"][1]) < 20){
         continue;
       }
-      this.wall = this.physics.add.image(coordinate['X'] * sizingRatio + offset, coordinate['Y'] * sizingRatio, 'wall');
+      group.create(coordinate['X'] * sizingRatio + offset, coordinate['Y'] * sizingRatio, 'wall');
+      /*
+      group.setImmovable(true);
+      group.setCircle(1);
+      group.setBounce(0);
+      group.checkCollision.up = true;
+      group.moves = false;
+      */
     }
+    group.refresh();
 
     this.marble = this.physics.add.image(coordinates["ball"][0] * sizingRatio + offset, coordinates["ball"][1] * sizingRatio, 'ball');
-    this.marble.setCircle(46);
-    this.marble.setFriction(0.005);
+    this.goal = this.physics.add.image(coordinates["hole"][0] * sizingRatio + offset, coordinates["hole"][1] * sizingRatio, 'hole');
+    this.marble.setCircle(15);
     this.marble.setCollideWorldBounds(true);
-    this.marble.setBounce(1);
-    //marble.setVelocity(150);
 
+    console.log(this.marble);
+    this.physics.add.collider(this.marble, group, function(marble){
+      if (marble.body.wasTouching.left || marble.body.touching.left){
+        marble.setVelocityX(1);
+      }
+    });
+
+    window.addEventListener("deviceorientation", this.handleOrientation.bind(this), true);
+    //this.marble.setBounce(1);
+    //marble.setVelocity(150);
   }
 
   //initLevels() {}
@@ -82,7 +103,9 @@ export default class GameScene extends Phaser.Scene {
   //wallCollision() {}
 
   handleOrientation(e) {
-
+    //if (e.gamma)
+    this.marble.setVelocityX(e.beta * 12)
+    this.marble.setVelocityY(e.gamma * -12)
   }
 
   //finishLevel() {}
